5cdd75a50a5a1180ee5d68328d73baafca001043
San77-art <santiagoribeirodelima@gmail.com>
2026-08-16 16:38:03 -0300

    Corrigir session.py - Exportar Base corretamente

--- app/database/session.py
+++ app/database/session.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+﻿from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:CernovaRB2026!@cernova-rb-db.c61cukey2jxy.us-east-1.rds.amazonaws.com:5432/cernova_rb")
 
 engine = create_engine(
     DATABASE_URL,
@@ -14,11 +14,9 @@ engine = create_engine(
     max_overflow=10
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
